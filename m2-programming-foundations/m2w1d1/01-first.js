e7258a26cfd98c52f732cbc2e8ef5dffe5532ef3
henri-kapidani <97576915+henri-kapidani@users.noreply.github.com>
2025-10-14 06:48:08 +0200

    day 5

---
+++ m2-programming-foundations/m2w1d1/01-first.js
@@ -0,0 +1,18 @@
+let num1 = 10; // questo è il primo numero
+/*
+commento
+multilinea
+*/
+let num2 = 20;
+
+let result = num1 + num2;
+
+console.log(result);
+
+result = null; // vuoto intenzionale
+
+console.log(result);
+
+let myVar;
+
+console.log(myVar);
