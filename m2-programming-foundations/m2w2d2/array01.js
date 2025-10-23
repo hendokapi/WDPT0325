da962f0b118fe567e06e1131a45fa9e97c291855
henri-kapidani <97576915+henri-kapidani@users.noreply.github.com>
2025-10-23 21:43:27 +0200

    day 8

---
+++ m2-programming-foundations/m2w2d2/array01.js
@@ -0,0 +1,8 @@
+let arr = [1, '2', { three: 3 }, [4, 5]];
+
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[2].three);
+console.log(arr[3]);
+console.log(arr[3][1]);
