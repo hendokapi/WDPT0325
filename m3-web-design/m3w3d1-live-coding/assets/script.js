f6babb75978b4be7846b327bb4a810bfa1289658
henri-kapidani <97576915+henri-kapidani@users.noreply.github.com>
2025-12-01 21:39:01 +0100

    day 17 amazon prime

---
+++ m3-web-design/m3w3d1-live-coding/assets/script.js
@@ -0,0 +1,6 @@
+// setInterval(() => {
+//     const r = Math.floor(Math.random() * (255 + 1));
+//     const g = Math.floor(Math.random() * (255 + 1));
+//     const b = Math.floor(Math.random() * (255 + 1));
+//     document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+// }, 100);
