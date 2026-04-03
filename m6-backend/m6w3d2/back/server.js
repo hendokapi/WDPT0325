30bdcb492c45b5b530568080bfa801732ec9b116
henri-kapidani <97576915+henri-kapidani@users.noreply.github.com>
2026-04-03 05:37:08 +0200

    backend day 6 - authentication

---
+++ m6-backend/m6w3d2/back/server.js
@@ -0,0 +1,42 @@
+import 'dotenv/config';
+import cors from 'cors';
+import morgan from 'morgan';
+import express from 'express';
+import mongoose from 'mongoose';
+import userRouter from './routes/user.router.js';
+import recipeRouter from './routes/recipe.router.js';
+import commentRouter from './routes/comment.router.js';
+import authRouter from './routes/auth.router.js';
+
+const PORT = process.env.PORT;
+
+const server = express(); // creato il server base
+server.use(morgan('dev'));
+server.use(cors()); // serve a risolvere i problemi di CORS quando si collega l'api con il frontend
+server.use(express.json()); // serve ad accettare json nel body delle richieste
+
+server.use('/api/v1/users', userRouter);
+server.use('/api/v1/recipes', recipeRouter);
+server.use('/api/v1/recipes', commentRouter);
+server.use('/api/v1/auth', authRouter);
+
+// gestore di errori
+server.use((errorCode, request, response, next) => {
+    if (errorCode == 404) next();
+    else if (Number.isInteger(errorCode)) response.status(errorCode).send();
+    else response.status(500).send();
+});
+
+// gestore della 404
+server.use((request, response) => {
+    response.status(404).send({ message: 'resource not found' });
+});
+
+await mongoose
+    .connect(process.env.MONGODB_CONNECTION_URI)
+    .then(() => console.log('Database connesso'))
+    .catch((err) => console.log(err));
+
+server.listen(PORT, () => {
+    console.log(`Server in esecuzione alla porta ${PORT}`);
+});
---
+++ m6-backend/m6w3d2/front/src/components/Header.jsx
@@ -0,0 +1,49 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router';
+
+function Header() {
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary">
+            {/* <h2>
+                <a href="/about">Link sbagliato</a>
+            </h2>
+            <h2>
+                <Link to="/about">Link giusto</Link>
+            </h2> */}
+            <Container>
+                <Navbar.Brand as={Link} to="/">
+                    EpiPosts
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to="/about">
+                            About
+                        </Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">
+                                Action
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                Another action
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">
+                                Something
+                            </NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                                Separated link
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default Header;
